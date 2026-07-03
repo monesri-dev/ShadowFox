8e7ca65d0b5f52ec7bfbfa7b701e9ae74cc1c4be
Monesri <monesri846@gmail.com>
2026-07-03 21:43:13 +0530

    first portfolio upload

--- Portfolio-Website/script.js
+++ Portfolio-Website/script.js
@@ -1,1 +1,32 @@
+const text = ["Software Developer", "CSE Student", "Cloud Enthusiast"];
 
+let i = 0;
+let j = 0;
+let isDeleting = false;
+
+function type() {
+  let current = text[i];
+
+  if (!isDeleting) {
+    j++;
+  } else {
+    j--;
+  }
+
+  document.getElementById("typing").innerText = current.substring(0, j);
+
+  if (!isDeleting && j === current.length) {
+    isDeleting = true;
+    setTimeout(type, 1000);
+    return;
+  }
+
+  if (isDeleting && j === 0) {
+    isDeleting = false;
+    i = (i + 1) % text.length;
+  }
+
+  setTimeout(type, 100);
+}
+
+type();
